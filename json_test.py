8d7930b6f5fc02124b05cb313254070f70f7fbdd
Vitaliy Zhukov <zhuchog@me.com>
2013-02-21 17:38:01 +0800

    Begining learn parsing library

--- json_test.py
+++ json_test.py
@@ -1,18 +1,36 @@
-from flask import Flask, redirect, jsonify
-from redis import Redis
+# -*- coding: UTF-8 -*-
 
-app = Flask(__name__)
-app.debug = True
+import urllib2
+from bs4 import BeautifulSoup
 
-db = Redis()
+# from flask import Flask, redirect, jsonify
+# from redis import Redis
 
-@app.route('/')
-def hello():
-	return jsonify({'result': 'test', 'foto': 'test'})
+# app = Flask(__name__)
+# app.debug = True
 
-@app.route('/vote/<int:voteid>', methods=['POST'])
-def vote(voteid):
-	return jsonify({'result': 'test', 'foto': 'test'})
+# page = urllib.urlopen("test1.html")
 
-if __name__ == '__main__':
-	app.run()
+# doc = lxml.html.document_fromstring(page.read())
+
+# txt1 = doc.xpath('/html/body/center/table[5]/tbody/tr[3]/text()')
+
+soup = BeautifulSoup(urllib2.urlopen("http://forum.guns.ru/forummessage/92/507831.html"))
+
+#txt1 = doc.xpath('/html/body/span[@class="simple_text"]/text()')
+#txt2 = doc.xpath('/html/body/span[@class="cyrillic_text"]/following-sibling::text()[1]')
+
+txt = soup.find_all("td")
+
+# print txt
+
+for link in soup.find_all('td'):
+    print(link.get_text())
+
+# @app.route('/')
+# def hello():
+# 	return jsonify({'resulting': txt, 'foto': 'test'})
+
+
+# if __name__ == '__main__':
+# 	app.run()
